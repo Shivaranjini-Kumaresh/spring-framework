46e9b09b2e6d26e2ac80e6e41c7c8133e51f61ee
Shivaranjini <s.arumugaperumal@walmartlabs.com>
2018-08-07 16:30:59 +0530

    Added base structure for spring boot web app

---
+++ Projects/spring5WebApp/src/main/java/guru/springframework/spring5WebApp/model/Author.java
@@ -0,0 +1,47 @@
+package guru.springframework.spring5WebApp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private Long id;
+	private String authorName;
+
+	@ManyToMany(mappedBy ="authors")
+	private Set<Book> books = new HashSet<Book>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+}
---
+++ Projects/spring5WebApp/src/main/java/guru/springframework/spring5WebApp/model/Book.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring5WebApp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private Long id;
+	private String bookName;
+	private String genre;
+	private String publisher;
+
+	@ManyToMany
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+	private Set<Author> authors = new HashSet<Author>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+}
